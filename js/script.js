4c7aafb2cfa858a7dfb3be2f5c293b0f1dc4356c
patrictobola <tobola92@gmail.com>
2023-06-05 15:00:47 +0200

    list done with for loop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK')
+
+const listContainer = document.getElementById('list');
+
+const shoppingList = ['pane', 'pasta', 'acqua', 'birra', 'latte', 'uova', 'frutta', 'verdura', 'caffè', 'sale', 'carne', 'pesce', ];
+
+listItems = `<ul>`
+for (let i = 0; i < shoppingList.length; i++){
+    listItems += `<li>${shoppingList[i]}</li>`
+}
+listItems += `</ul>`
+
+listContainer.innerHTML = listItems;
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Lista della spesa
+
+- recupero elementi dal dom
+- creo un array con la lista della spesa dentro
+- creo un ciclo 'for' che stampi in pagina ogni elemento della lista
+
+- riadatto il ciclo for al ciclo while
